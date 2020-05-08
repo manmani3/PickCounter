df669fada72e8021fb6c910dc5986a18f01218c4
unknown <ezer@ajou.ac.kr>
2020-05-08 23:36:07 +0900

    classification image capture function

---
+++ UiCapture.py
@@ -0,0 +1,73 @@
+import win32gui
+from PIL import ImageGrab, Image
+
+def captureClient() :
+    
+    window = win32gui.FindWindow(None, r'League of Legends')
+    win32gui.SetForegroundWindow(window)
+
+    position = win32gui.GetWindowRect(window)
+
+    image = ImageGrab.grab(position)
+
+    return image
+
+def cropImages(image) :
+
+    width, height = image.size
+    print(width, height)
+
+    p_1600 = {'start_x' : 83, 'start_y' : 144, 'size' : 50, 'margin' : 100, 'enemy_x': 1504}
+    p_1280 = {'start_x' : 66, 'start_y' : 116, 'size' : 36, 'margin' : 80, 'enemy_x': 1203}
+    p_1024 = {'start_x' : 53, 'start_y' : 92, 'size' : 32, 'margin' : 64, 'enemy_x': 963}
+    
+    b_1600 = {'start_x' : 20, 'start_y' : 40, 'size' : 33, 'margin' : 50, 'enemy_x': 1346}
+    b_1280 = {'start_x' : 17, 'start_y' : 32, 'size' : 26, 'margin' : 40, 'enemy_x': 1078}
+    b_1024 = {'start_x' : 16, 'start_y' : 24, 'size' : 20, 'margin' : 32, 'enemy_x': 863}
+
+    our_p = list()
+    our_b = list()
+    enemy_p = list()
+    enemy_b = list()
+
+    if width == 1600 and height == 900 :
+        p_pos = p_1600
+        b_pos = b_1600
+
+    elif width == 1280 and height == 768 :
+        p_pos = p_1280
+        b_pos = b_1280
+        
+    elif width == 1024 and height == 576 :
+        p_pos = p_1024
+        b_pos = b_1280
+        
+    else :
+        return -1
+
+    for i in range(0, 5) :
+        our_p.insert(i, image.crop((p_pos['start_x'],
+                                    p_pos['start_y'],
+                                    p_pos['start_x'] + p_pos['size'],
+                                    p_pos['start_y'] + p_pos['size'])))
+                                    
+        our_b.insert(i, image.crop((b_pos['start_x'],
+                                    b_pos['start_y'],
+                                    b_pos['start_x'] + b_pos['size'],
+                                    b_pos['start_y'] + b_pos['size'])))
+
+        enemy_p.insert(i, image.crop((b_pos['enemy_x'],
+                                      b_pos['start_y'],
+                                      b_pos['enemy_x'] + b_pos['size'],
+                                      b_pos['start_y'] + b_pos['size'])))
+
+        enemy_p.insert(i, image.crop((b_pos['enemy_x'],
+                                      b_pos['start_y'],
+                                      b_pos['enemy_x'] + b_pos['size'],
+                                      b_pos['start_y'] + b_pos['size'])))
+        
+        p_pos['start_y'] += p_pos['margin']
+        b_pos['start_x'] += p_pos['margin']
+        b_pos['enemy_x'] += p_pos['margin']
+
+    return our_p, our_b, enemy_p, enemy_b
--- UiDialog.py
+++ UiDialog.py
@@ -55,54 +55,11 @@ class UiDialog(QWidget):
         ourBanList, ourPickList, yourBanList, yourPickList \
             = self.getChampNames(ourBanList, ourPickList, yourBanList, yourPickList)
 
-
-
-    # return ImageFile
-    def captureClient(self):
-        client = win32gui.FindWindow(None, r'League of Legends')
-        win32gui.SetForegroundWindow(client)
-        dimensions = win32gui.GetWindowRect(client)
-        capture = ImageGrab.grab(dimensions)
-        width, height = capture.size
-        return capture, width, height
-
-    # return 4 Array<ImageFile>
-    def cropImages(self, imageFile, width, height):
-        pickPos1600 = [83, 144, 50, 50]
-        banPos1600 = [20, 40, 33, 33]
-
-        #testvalue
-        pickPosTest = [0, 0, 50, 50]
-        banPosTest = [0, 0, 33, 33]
-
-        ourPicks, ourBans, yourPicks, yourBans = []
-        pickMargin, banMargin = 0
-
-        if width == 1600 and height == 900:
-            pickPos = pickPos1600
-            banPos = banPos1600
-        else :
-            #window size not found #testvalue
-            pickPos = pickPosTest
-            banPos = banPosTest
-
-        for i in range(0, 5) :
-            pickPos[1] = pickPos[1] + pickMargin #margin added START_Y
-            banPos[0] = banPos[0] + banMargin #margin added START_X
-
-            ourPicks[i] = imageFile.crop((pickPos[0], pickPos[1], pickPos[2], pickPos[3]))
-            ourBans[i] = imageFile.crop((banPos[0], banPos[1], banPos[2], banPos[3]))
-            #yourPicks[i] = client.crop(()) #? need to check position
-            #yourBans[i] = client.crop(())
-        return ourPicks, ourBans, yourPicks, yourBans
-
     # paremeters : 4 Array<ImageFile>
     # return : 4 Array<String>
     def getChampNames(self, ourBanList, ourPickList, yourBanList, yourPickList):
         response = requests.get(url)
         img = Image.open(BytesIO(response.content))
-
-
 
 
 if __name__ == "__main__":
